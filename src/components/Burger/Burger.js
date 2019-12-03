898e33f24c0fbea35a93c6c952c1a77c75c5eab6
Ed Wilgosz <EdWilgosz@gmail.com>
2019-12-03 18:08:35 -0500

    Build order page

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import classes from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient'
+
+const burger = props => {
+    let transformedIngredients = Object.keys(props.ingredients)
+    .map(ingKey => {
+        return [...Array(props.ingredients[ingKey])].map((_, index) => {
+            return <BurgerIngredient key={ingKey + index} type={ingKey} />;
+        } );
+    } )
+        .reduce((arr, cur) => {
+            return arr.concat(cur);
+    }, []);
+
+    if(transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients!</p>
+    }
+
+    return (
+        <div className={classes.Burger}>
+            <BurgerIngredient type="bread-top" />
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom" />
+        </div>
+    );  
+};
+
+export default burger;
